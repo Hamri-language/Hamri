484e75ab47e8a8302d4aa53ae327983af22f5aac
Hamri Programming Language <133820505+Hamri-language@users.noreply.github.com>
2023-05-19 15:46:24 +0300

    added comments,sample hamri script and path variable

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from LexicalParser import LexicalParser
 from StatementParser import StatementParser
 from Logger import Logs,LogKeys
-path_ = '/home/bytefrost/Documents/Hamri/Projects/test.ham'
+path_ = 'path_to_hamri_file'
+
+##Note: hamri script files end in the extension ' .ham '
+
+##Here is a sample Hamri script
+
+'''
+
+kwanza
+
+    hamri_v = "Hamri v1.02"
+
+    eleza sayHi(name)
+
+        chapa "Hello world. " + "I am " + name
+
+    kwisha
+
+
+    sayHi(hamri_v)
+
+    chapa 10 / 2
+
+    aina(hamri_v)
+
+
+kwisha
+
+
+
+'''
 
 
 if __name__ == '__main__':
     lexicalParser = LexicalParser(path_).parse()
-    lexicalParser.print_tokens()
+    #print to console all the tokens found in script
+    #lexicalParser.print_tokens() 
     statementParser = StatementParser(lexicalParser.token_list).parse()
-    statementParser.print_statements()
+    #print to console all the statements parsed from the tokens found
+    #statementParser.print_statements()
     statementParser.execute()
     
     # For execution log:
