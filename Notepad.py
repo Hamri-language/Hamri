247e15773ba656ba27573885913477418e4c5925
Fredrick Omollo <omollofd@gmail.com>
2023-05-20 06:29:11 +0300

    update Notepad.py to use console

--- Notepad.py
+++ Notepad.py
@@ -1,25 +1,23 @@
-import tkinter
-import os
-import webbrowser
+import tkinter as tk
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
-import tkinter as tk
-import re
+import webbrowser
 from tkinter import ttk
+import os
 
 
-import LexicalParser,StatementParser
-
-# Ubuntu 20.0.1 sudo apt install python3.8-tk for tkinter module
+from LexicalParser import LexicalParser
+from StatementParser import StatementParser
+from Console import console
+from Logger import Logs,LogKeys
 
 class CustomText(Text):
-    
     def __init__(self, *args, **kwargs):
-        """A text widget that report on internal widget commands"""
-        Text.__init__(self, *args, **kwargs)
+        """A text widget that reports on internal widget commands"""
+        super().__init__(*args, **kwargs)
 
-        # create a proxy for the underlying widget
+        # Create a proxy for the underlying widget
         self._orig = self._w + "_orig"
         self.tk.call("rename", self._w, self._orig)
         self.tk.createcommand(self._w, self._proxy)
@@ -35,208 +33,172 @@ class CustomText(Text):
 
 
 class Notepad:
+    def __init__(self, **kwargs):
+        self.__root = Tk()
+        self.__thisWidth = kwargs.get('width', 300)
+        self.__thisHeight = kwargs.get('height', 300)
+        self.__thisTitle = 'Hamri IDE'
+        self.__thisDefaultDir = '/home/bytefrost/Documents/Hamri/Projects/'
+        self.__thisFile = None
+        self.__Tokens = None
 
-    __root = Tk()
-    # default window width and height
-    __Tokens = None
-    __thisWidth = 300
-    __thisTitle = 'Hamri IDE'
-    __thisDefaultDir = '/home/bytefrost/Documents/Hamri/Projects/'
-    __thisCodeFrame = None
-    __thisConsoleFrame = None
-    __thisHeight = 300
-    __thisTextArea = None
-    __thisConsole = None
-    __thisMenuBar = Menu(__root)
-    __thisFileMenu = Menu(__thisMenuBar, tearoff=0)
-    __thisEditMenu = Menu(__thisMenuBar, tearoff=0)
-    __thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
-    # To add scrollbar
-    __thisScrollBar = None
-    __thisConsoleScrollBar = None
-    __file = None
-
-    def __init__(self,**kwargs):
-        # Set icon
-        try:
-            self.__root.wm_iconbitmap("Notepad.ico")
-        except:
-            pass
-        # Set window size (the default is 300x300)
-        try:
-            self.__thisWidth = kwargs['width']
-        except KeyError:
-            pass
-
-        try:
-            self.__thisHeight = kwargs['height']
-        except KeyError:
-            pass
-        
-        
-
-        # Set the window text
         self.__root.title(self.__thisTitle)
-        
-        # Center the window
-        screenWidth = self.__root.winfo_screenwidth()
-        screenHeight = self.__root.winfo_screenheight()
-        # For left-alling
-        left = (screenWidth / 2) - (self.__thisWidth / 2)
-        # For right-allign
-        top = (screenHeight / 2) - (self.__thisHeight /2)
-        # For top and bottom
+        self.__root.geometry(f"{self.__thisWidth}x{self.__thisHeight}")
+        self.__root.resizable(1, 1)
 
-        self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth,self.__thisHeight,left, top))
-        # To make the textarea auto resizable
-        
-        self.__root.resizable(1,1)
-        
+        # Create the code frame
+        self.__thisCodeFrame = ttk.Frame(self.__root)
+        self.__thisCodeFrame.pack(fill='both', expand=1)
 
-        # Add controls (widget)
-        
-        self.__thisCodeFrame = ttk.Frame(self.__root)    
-                 
-        self.__thisCodeFrame.pack(fill='both',expand=1)
-        
-        self.__thisTextArea = CustomText(self.__thisCodeFrame,font=("Courier", 12, "normal"))
-        
-        self.__thisTextArea.pack(side=TOP,fill='both',expand=1)     
-        
-        self.__thisConsole = CustomText(self.__thisCodeFrame,font=("Courier", 12, "normal"),height=18,bg="black",fg="white")
-        self.__thisScrollBar = Scrollbar(self.__thisTextArea)
-        self.__thisConsole.pack(side=TOP,fill='x')
-        
-        
-        # To open new file
-        self.__thisFileMenu.add_command(label="New",
-                                                command=self.__newFile)
-        # To open a already existing file
-        self.__thisFileMenu.add_command(label="Open",
-                                                command=self.__openFile)
-        # To save current file
-        self.__thisFileMenu.add_command(label="Save",
-                                                command=self.__saveFile)
-        #To execute current script
-        self.__thisFileMenu.add_command(label="Execute",command=self.__execute)
-        
-        # To create a line in the dialog
-        self.__thisFileMenu.add_separator()
-        self.__thisFileMenu.add_command(label="Exit",
-                                                command=self.__quitApplication)
-        self.__thisMenuBar.add_cascade(label="File",
-                                               menu=self.__thisFileMenu)
-        # To give a feature of cut
-        self.__thisEditMenu.add_command(label="Cut",
-                                                command=self.__cut)
-        # to give a feature of copy
-        self.__thisEditMenu.add_command(label="Copy",
-                                                command=self.__copy)
-        # To give a feature of paste
-        self.__thisEditMenu.add_command(label="Paste",
-                                                command=self.__paste)
-        # To give a feature of editing
-        self.__thisMenuBar.add_cascade(label="Edit",
-                                               menu=self.__thisEditMenu)
-        # To create a feature of description of the notepad
-        self.__thisHelpMenu.add_command(label="View Help",
-                                                command=self.__showAbout)
-        self.__thisMenuBar.add_cascade(label="Help",
-                                               menu=self.__thisHelpMenu)
+        # Create the text area for code editing
+        self.__thisTextArea = CustomText(self.__thisCodeFrame, font=("Courier", 12, "normal"))
+        self.__thisTextArea.pack(side=TOP, fill='both', expand=1)
+
+        # Create the console for displaying output
+        self.__thisConsole = CustomText(self.__thisCodeFrame, font=("Courier", 12, "normal"), height=18, bg="black", fg="white")
+        self.__thisConsole.pack(side=TOP, fill='x')
+
+        # Create the menu bar
+        self.__thisMenuBar = Menu(self.__root)
         self.__root.config(menu=self.__thisMenuBar)
-        #self.__thisScrollBar.pack(side=RIGHT,fill=Y)
-        # Scrollbar will adjust automatically according to the content
-        #self.__thisScrollBar.config(command=self.__thisTextArea.yview)
-        #self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
-    def __quitApplication(self):
-        self.__root.destroy()
-        # exit()
-    def __showAbout(self):
-        showinfo("Notepad",view =webbrowser.open("https://www.bing.com/search?q=get+help+with+notepad+in+windows+10&filters=guid:%224466414-en-dia%22%20lang:%22en%22&form=T00032&ocid=HelpPane-BingIA"))
-    def __openFile(self):
-        self.__file = askopenfilename(initialdir=self.__thisDefaultDir,defaultextension=".ham",filetypes=[("Hamri script","*.ham"),("Text Documents","*.txt")])
-        if self.__file == "":
-            # no file to open
-            self.__file = None
-        else:
-            # Try to open the file
-            # set the window title
-            self.__root.title(self.__thisTitle +' - '+os.path.basename(self.__file))
-            self.__thisTextArea.delete(1.0,END)
-            file = open(self.__file,"r")
-            self.__Tokens = LexicalParser.LexicalParser(self.__file).parse()
-            self.__thisTextArea.bind("<<TextModified>>",self.__generateTags)
-            self.__thisTextArea.insert(1.0,file.read())
-            file.close()
-            
-            
-    def __generateTags(self,event):
-        
-        if type(self.__file) == str and self.__file != '':
-            
-            tags_dir = {"keyword":"blue"}
-            for i in self.__Tokens.token_list:
-                if i.token_type in tags_dir.keys():
-                    
-                    self.__thisTextArea.tag_add('{}-{}'.format(i.token_type,i.position()), "{}.{}".format(i.line,i.start), "{}.{}".format(i.line,i.len_))
-                    self.__thisTextArea.tag_config('{}-{}'.format(i.token_type,i.position()), foreground=tags_dir[i.token_type],font=("Courier", 11, "bold"))
 
+        # Create the File menu
+        self.__thisFileMenu = Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisFileMenu.add_command(label="New", command=self.__newFile)
+        self.__thisFileMenu.add_command(label="Open", command=self.__openFile)
+        self.__thisFileMenu.add_command(label="Save", command=self.__saveFile)
+        self.__thisFileMenu.add_command(label="Execute", command=self.__execute)
+        self.__thisFileMenu.add_separator()
+        self.__thisFileMenu.add_command(label="Exit", command=self.__quitApplication)
+        self.__thisMenuBar.add_cascade(label="File", menu=self.__thisFileMenu)
+
+        # Create the Edit menu
+        self.__thisEditMenu = Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisEditMenu.add_command(label="Cut", command=self.__cut)
+        self.__thisEditMenu.add_command(label="Copy", command=self.__copy)
+        self.__thisEditMenu.add_command(label="Paste", command=self.__paste)
+        self.__thisMenuBar.add_cascade(label="Edit", menu=self.__thisEditMenu)
+
+        # Create the Help menu
+        self.__thisHelpMenu = Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisHelpMenu.add_command(label="About Hamri IDE", command=self.__showAbout)
+        self.__thisHelpMenu.add_command(label="Open Hamri Documentation", command=self.__openDocumentation)
+        self.__thisMenuBar.add_cascade(label="Help", menu=self.__thisHelpMenu)
+
+        # Bind events to the text area
+        self.__thisTextArea.bind("<<TextModified>>", self.__generateTags)
+
+    def __generateTags(self, event=None):
+        """Generates tags for syntax highlighting"""
+        # Clear existing tags
+        self.__thisTextArea.tag_delete("Token.Keyword")
+        self.__thisTextArea.tag_delete("Token.Identifier")
+        self.__thisTextArea.tag_delete("Token.Operator")
+        self.__thisTextArea.tag_delete("Token.Literal")
+
+        # Parse code and generate tokens
+        code = self.__thisTextArea.get("1.0", "end-1c")
+        lexer = LexicalParser(code).parse()
+        self.__Tokens = lexer.token_list
+        
+
+        # Configure tags for different token types
+        for token in self.__Tokens:
+            if token.type == "Keyword":
+                self.__thisTextArea.tag_add("Token.Keyword", token.start, token.end)
+                self.__thisTextArea.tag_configure("Token.Keyword", foreground="blue", font=("Courier", 12, "bold"))
+            elif token.type == "Identifier":
+                self.__thisTextArea.tag_add("Token.Identifier", token.start, token.end)
+                self.__thisTextArea.tag_configure("Token.Identifier", foreground="black", font=("Courier", 12, "normal"))
+            elif token.type == "Operator":
+                self.__thisTextArea.tag_add("Token.Operator", token.start, token.end)
+                self.__thisTextArea.tag_configure("Token.Operator", foreground="green", font=("Courier", 12, "normal"))
+            elif token.type == "Literal":
+                self.__thisTextArea.tag_add("Token.Literal", token.start, token.end)
+                self.__thisTextArea.tag_configure("Token.Literal", foreground="purple", font=("Courier", 12, "normal"))
 
     def __newFile(self):
-        self.__root.title("Untitled - Notepad")
-        self.__file = None
-        self.__thisTextArea.delete(1.0,END)
-        
-    def __execute(self):
-        
-        self.__thisConsole.delete(1.0,END)
-        
-        if self.__Tokens != None:
-            self.__Tokens = LexicalParser.LexicalParser(self.__file).parse()
-            statementParser = StatementParser.StatementParser(self.__Tokens.token_list).parse(self.__thisConsole)
-            statementParser.execute()
-            
-            
+        """Create a new file"""
+        self.__root.title("Untitled - Hamri IDE")
+        self.__thisFile = None
+        self.__thisTextArea.delete("1.0", "end")
+
+    def __openFile(self):
+        """Open an existing file"""
+        self.__thisFile = askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+
+        if self.__thisFile:
+            self.__root.title(f"{os.path.basename(self.__thisFile)} - Hamri IDE")
+            self.__thisTextArea.delete("1.0", "end")
+
+            with open(self.__thisFile, "r") as file:
+                self.__thisTextArea.insert("1.0", file.read())
 
     def __saveFile(self):
-
-        if self.__file == None:
-            # Save as new file
-            self.__file = asksaveasfilename(initialfile='Untitled.txt',
-                                                        defaultextension=".txt",
-                                                                                        filetypes=[("All Files","*.*"),
-                                                                                                   ("Text Documents","*.txt")])
-
-            if self.__file == "":
-                self.__file = None
-            else:
-                # Try to save the file
-                file = open(self.__file,"Untitled")
-                file.write(self.__thisTextArea.get(1.0,END))
-                file.close()
-
-                # Change the window title
-                self.__root.title(os.path.basename(self.__file) + " - Notepad")
-
+        """Save the current file"""
+        if self.__thisFile:
+            with open(self.__thisFile, "w") as file:
+                file.write(self.__thisTextArea.get("1.0", "end-1c"))
         else:
-            file = open(self.__file,"w")
-            file.write(self.__thisTextArea.get(1.0,END))
-            file.close()
+            self.__saveFileAs()
+
+    def __saveFileAs(self):
+        """Save the current file with a new name"""
+        self.__thisFile = asksaveasfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+
+        if self.__thisFile:
+            with open(self.__thisFile, "w") as file:
+                file.write(self.__thisTextArea.get("1.0", "end-1c"))
+            self.__root.title(f"{os.path.basename(self.__thisFile)} - Hamri IDE")
 
     def __cut(self):
+        """Cut the selected text"""
         self.__thisTextArea.event_generate("<<Cut>>")
 
     def __copy(self):
+        """Copy the selected text"""
         self.__thisTextArea.event_generate("<<Copy>>")
 
     def __paste(self):
+        """Paste the clipboard content"""
         self.__thisTextArea.event_generate("<<Paste>>")
 
-    def run(self):
-        # Run main application
-        self.__root.mainloop()
-        
+    def __execute(self):
+        """Execute the code and display output in the console"""
+        code = self.__thisTextArea.get("1.0", "end-1c")
 
-# Run main application
-notepad = Notepad(width=700,height=600)
+        # Perform lexical analysis
+        lexer = LexicalParser(code).parse()
+        tokens = lexer.token_list
+
+        # Perform statement parsing
+        statements = StatementParser(tokens).parse()
+        
+        #pass our console object to the runtime sequence and clear it
+        
+        console.use_console(self.__thisConsole)
+
+        # Execute the statements and capture the output
+        
+        result = statements.execute()
+
+    def __quitApplication(self):
+        """Quit the application"""
+        self.__root.destroy()
+
+    def __showAbout(self):
+        """Display information about the application"""
+        showinfo("Hamri IDE", "A simple IDE for the Hamri programming language.")
+
+    def __openDocumentation(self):
+        """Open the Hamri documentation in a web browser"""
+        webbrowser.open("https://example.com/hamri-documentation")
+
+    def run(self):
+        """Run the application"""
+        self.__root.mainloop()
+
+
+# Create an instance of the Notepad class and run the application
+notepad = Notepad(width=800, height=600)
 notepad.run()
